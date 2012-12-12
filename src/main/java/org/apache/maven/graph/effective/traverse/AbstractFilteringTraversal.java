483b643923e271b28bcdb848440eae2b7dc8b74e
John Casey <jdcasey@commonjava.org>
2012-12-12 00:09:40 -0600

    Apply ASL2 copyright notices, remove sysout from AbstractFilteringTraversal

--- src/main/java/org/apache/maven/graph/effective/traverse/AbstractFilteringTraversal.java
+++ src/main/java/org/apache/maven/graph/effective/traverse/AbstractFilteringTraversal.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2012 John Casey
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package org.apache.maven.graph.effective.traverse;
 
 import java.util.HashMap;
@@ -43,8 +58,6 @@ public abstract class AbstractFilteringTraversal
     public final void edgeTraversed( final ProjectRelationship<?> relationship,
                                      final List<ProjectRelationship<?>> path, final int pass )
     {
-        System.out.println( "POP: " + relationship );
-
         final EdgeId id = new EdgeId( relationship, path );
         final ProjectRelationshipFilter filter = filtersInReserve.get( id );
 
@@ -68,8 +81,6 @@ public abstract class AbstractFilteringTraversal
             {
                 final EdgeId id = new EdgeId( relationship, path );
                 filtersInReserve.put( id, currentFilter );
-
-                System.out.println( "PUSH: " + relationship );
 
                 currentFilter = currentFilter.getChildFilter( relationship );
             }
